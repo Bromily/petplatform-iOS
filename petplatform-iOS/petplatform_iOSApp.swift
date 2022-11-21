c1c8de7495efcbbe0f0ce06728b27f4ffc4392da
haksung59 <hak9501@gmail.com>
2022-11-21 22:09:07 +0900

    create project

---
+++ petplatform-iOS/petplatform_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  petplatform_iOSApp.swift
+//  petplatform-iOS
+//
+//  Created by haksung on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct petplatform_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
